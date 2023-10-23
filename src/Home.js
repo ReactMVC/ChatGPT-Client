958f169f86b7c762d53a08dae3b99a51e3f28478
Hossein Pira <120723120+ReactMVC@users.noreply.github.com>
2023-10-23 06:01:50 -0400

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Linking, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Home = ({ navigation }) => {
@@ -16,6 +16,16 @@ const Home = ({ navigation }) => {
             <TouchableOpacity style={styles.button} onPress={() => Alert('Coming Soon', 'This section will be activated soon...')}>
                 <Icon name="image" size={30} color="#000" />
                 <Text style={styles.buttonText}>Image Generation</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('market://details?id=com.chatbot.pira')}>
+                <Icon name="star" size={30} color="#000" />
+                <Text style={styles.buttonText}>Rate App</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => Linking.openURL('https://github.com/ReactMVC/ChatGPT-Client')}>
+                <Icon name="github" size={30} color="#000" />
+                <Text style={styles.buttonText}>Github</Text>
             </TouchableOpacity>
 
             <Text style={styles.footer}>Developer: Hossein Pira</Text>
@@ -37,14 +47,19 @@ const styles = StyleSheet.create({
     subtitle: {
         fontSize: 20,
         color: '#666',
+        marginTop: '10px',
+        marginBottom: '8px',
     },
     button: {
         flexDirection: 'row',
         alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
         backgroundColor: '#ddd',
         padding: 10,
         margin: 10,
         borderRadius: 5,
+        alignSelf: 'stretch',
     },
     buttonText: {
         marginLeft: 10,
